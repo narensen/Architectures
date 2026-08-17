4a5606077154416fd9da8a192bfef84b408aa655
Naren Sengodan <scartegy@gmail.com>
2026-08-17 11:58:33 +0530

    Info : Returns context vector

--- SelfAttention.py
+++ SelfAttention.py
@@ -1,6 +1,15 @@
 import torch.nn as nn
+import numpy
 import torch
 
+inputs = torch.tensor(
+[[0.43, 0.15, 0.89], # Your
+[0.55, 0.87, 0.66], # journey
+[0.57, 0.85, 0.64], # starts
+[0.22, 0.58, 0.33], # with
+[0.77, 0.25, 0.10], # one
+[0.05, 0.80, 0.55]] # step
+)
 
 class SelfAttention_v2(nn.Module):
 
@@ -12,10 +21,17 @@ class SelfAttention_v2(nn.Module):
         self.W_value = nn.Linear(d_in, d_out, bias=False)
 
     def forward(self, x):
-        keys = x @ self.W_key
-        queries = x @ self.W_query
-        values = x @ self.W_value
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
         attn_scores = queries @ keys.T
         attn_weights = torch.softmax(attn_scores / keys.shape[1] ** 0.5, dim=-1)
         context_vec = attn_weights @ values
         return context_vec
+
+d_in = inputs.shape[1]
+d_out = 2
+
+torch.manual_seed(789)
+sa_v2 = SelfAttention_v2(d_in, d_out)
+print(sa_v2(inputs))
