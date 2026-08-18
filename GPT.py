2e5e13a1868dd53473dd167ded883cfef4e96946
Naren Sengodan <scartegy@gmail.com>
2026-08-18 17:49:52 +0530

    Info : FFN and Plot

--- GPT.py
+++ GPT.py
@@ -52,12 +52,25 @@ class LayerNorm(nn.Module):
 
 
 class GELU(nn.Module):
-    
+
     def __init__(self):
         super().__init__()
 
     def forward(self, x):
 
         return 0.5 * x * (1 + torch.tanh(torch.sqrt(torch.tensor(2.0 / torch.pi))
-                            * (x + 0.044715 * torch.pow(x, 3)))
+        * (x + 0.044715 * torch.pow(x, 3)))
         )
+
+class FeedForward(nn.Module):
+
+    def __init__(self, cfg, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
+            GELU(),
+            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"])
+        )
+
+    def forward(self, x):
+        return self.layers(x)
--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 from tokenizer import batch
-from GPT import GPT2, LayerNorm
+from GPT import GPT2, LayerNorm, FeedForward
 import torch.nn as nn
 import torch
 from config import GPT_CONFIG_124M as cfg
 
-"""torch.manual_seed(123)
-model = GPT2(cfg)
-logits = model(batch)
-print(logits)"""
-
-batch = torch.randn(2,4)
-
-ln = LayerNorm(emb_dim=4)
-out_ln = ln(batch)
-mean = out_ln.mean(dim=-1, keepdim = True)
-var = out_ln.var(dim=-1, keepdim=True, unbiased=False)
-print(mean, var)
+ffn = FeedForward(cfg)
+x = torch.rand(2, 3 , 768)
+out = ffn(x)
+print(out.shape)
+print(out)
