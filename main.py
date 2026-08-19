5b3f8465f8628ab5aec685f7caa75158d3dea04c
Naren Sengodan <scartegy@gmail.com>
2026-08-19 16:02:37 +0530

    Info : Generating Text from Untrained Language Model

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 from tokenizer import batch
 from classes import GPTBackbone, LayerNorm, FeedForward, TransformerBlock
 from GPT2 import GPT2
+from tokenizer import tokenzier
 import torch.nn as nn
 import torch
 from config import GPT_CONFIG_124M as cfg
+from services import *
 
+start_context = "Hello I am"
+encoded = tokenzier.encode(start_context)
+print(encoded)
+encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+print(encoded_tensor.shape)
 
 model = GPT2(cfg)
 
-total_params = sum(p.numel() for p in model.parameters())
-print(f"Total number of parameters: {total_params:,}")
+
+model.eval()
+
+out = generate_text_simple(
+    model=model,
+    idx=encoded_tensor,
+    max_new_tokens=6,
+    context_size=cfg["context_length"]
+    )
+
+print("Output:", out)
+print("Output length:", len(out[0]))
+
+decoded_text = tokenzier.decode(out.squeeze(0).tolist())
+print(decoded_text)
